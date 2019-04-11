74831629def70ae8781b2b73a08c785da043b46e
Bienz <Simon.Bienz@q-perior.com>
2019-04-11 17:51:12 +0200

    last commit of the day

---
+++ 2.OptionalJavaScript-AQuickRefresher/19PromiseStatement.js
@@ -0,0 +1,18 @@
+const promise = new Promise((resolve,reject) => {
+
+
+
+}
+
+
+)
+
+
+const fetchData = () => {
+    const promise = new Promise((resolve, reject) => {
+      setTimeout(() => {
+     console.log('Done!');
+      }, 15000);
+    });
+  };
+console.log('finish');
---
+++ 2.OptionalJavaScript-AQuickRefresher/19theThenStatemnt.js
@@ -0,0 +1,16 @@
+const wait1 = (fn) => {
+return new Promise (resolve => setTimeout(resolve, 100))
+}
+const wait2 = (fn) => {
+    return new Promise (resolve => setTimeout(resolve, 200))
+    }
+
+const wait3 = (fn) => {
+        return new Promise (resolve => setTimeout(resolve, 300))
+    }
+wait1()
+.then(wait2)
+.then(wait3)
+.then(function() {
+console.log('all good');
+});
