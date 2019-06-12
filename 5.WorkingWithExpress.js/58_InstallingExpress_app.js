cc4e057f7db90ad905245b525bea36b9cc52d3a1
Bienz <Simon.Bienz@q-perior.com>
2019-06-12 10:13:32 +0200

    10:00 contribution

--- 5.WorkingWithExpress.js/58_InstallingExpress_app.js
+++ 5.WorkingWithExpress.js/58_InstallingExpress_app.js
@@ -1,6 +1,17 @@
 const http = require('http');
+const express = require('express')
 
-console.log(routes.someText);
+const app = express();
+//express() can be written as function ;) for more details click crt & click on 'express' to go to the source
+//between app and server methods of express.js can be used e.g. app.listen
+app.use((req, res, next) => {
+    console.log('In the middleware');
+});
+//req = request, res = response, next is here to pass the response to the next middelwares
+app.use((req, res, next) => {
+    console.log('In another middleware');
+
+});
 
 const server = http.createServer(); 
 
